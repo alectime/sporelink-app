eda7bf05ae2f72db39197e924e9d625e789648d5
alectime <amczepiel@gmail.com>
2025-02-07 11:45:22 -0500

    Add .gitignore and environment configuration

--- src/utils/firebaseConfig.js
+++ src/utils/firebaseConfig.js
@@ -3,15 +3,26 @@ import { getAuth, initializeAuth, getReactNativePersistence } from "firebase/aut
 import { getFirestore } from "firebase/firestore";
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 import { Platform } from 'react-native';
+import Constants from 'expo-constants';
+
+const {
+  FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID,
+  FIREBASE_MEASUREMENT_ID,
+} = Constants.manifest?.extra || {};
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCqcAV6h7oFCmR6EvzFPnNuXwtxNTEaaM8",
-  authDomain: "sporelinkapp.firebaseapp.com",
-  projectId: "sporelinkapp",
-  storageBucket: "sporelinkapp.firebasestorage.app",
-  messagingSenderId: "852787405966",
-  appId: "1:852787405966:web:acf3a876ea16cb8ef4d5df",
-  measurementId: "G-57L8N8RD3C"
+  apiKey: FIREBASE_API_KEY || "AIzaSyCqcAV6h7oFCmR6EvzFPnNuXwtxNTEaaM8",
+  authDomain: FIREBASE_AUTH_DOMAIN || "sporelinkapp.firebaseapp.com",
+  projectId: FIREBASE_PROJECT_ID || "sporelinkapp",
+  storageBucket: FIREBASE_STORAGE_BUCKET || "sporelinkapp.firebasestorage.app",
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID || "852787405966",
+  appId: FIREBASE_APP_ID || "1:852787405966:web:acf3a876ea16cb8ef4d5df",
+  measurementId: FIREBASE_MEASUREMENT_ID || "G-57L8N8RD3C"
 };
 
 // Initialize Firebase
